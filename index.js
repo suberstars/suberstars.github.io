fdd45fd6cdd7516af42fd00db8896deccdae9da6
Kristen Colleen Dine <krisellees@gmail.com>
2023-03-06 22:14:51 +0000

    webpage building

---
+++ index.js
@@ -0,0 +1,7 @@
+let home = document.getElementById("home");
+let guides = document.getElementById("guides");
+let resources = document.getElementById("resources");
+let servers = document.getElementById("servers");
+
+home.addEventListener(onclick, window.location.href="/index.html");
+guides.addEventListener(onclick, window.location.href="/guides.html");
